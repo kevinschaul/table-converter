61510df64be9fe5fa0f3df21d6574c5b0a81adcf
Kevin Schaul <kevin.schaul@gmail.com>
2012-06-22 12:13:24 -0700

    Even/odd classes for html table

--- js/DataGridRenderer.js
+++ js/DataGridRenderer.js
@@ -72,102 +72,68 @@ var DataGridRenderer = {
     return outputText;
   },
   
-  
   //---------------------------------------
   // HTML Table
   //---------------------------------------
-  
-  html: function (dataGrid, headerNames, headerTypes, indent, newLine) {
+ 
+    html: function(
+            dataGrid, headerNames, headerTypes, indent, newLine) {
     //inits...
     var commentLine = "<!--";
     var commentLineEnd = "-->";
     var outputText = "";
     var numRows = dataGrid.length;
     var numColumns = headerNames.length;
-    
+
     //begin render loop
-    outputText += "<table>"+newLine;
-    outputText += indent+"<thead>"+newLine;
-    outputText += indent+indent+"<tr>"+newLine;
-    
-    for (var j=0; j < numColumns; j++) {
-      outputText += indent+indent+indent+'<th class="'+headerNames[j]+'-cell">';          
-      outputText += headerNames[j];
-      outputText += '</th>'+newLine;
+    outputText += "<table>" + newLine;
+    outputText += indent + "<thead>" + newLine;
+    outputText += indent + indent + "<tr>" + newLine;
+
+    for (var j = 0; j < numColumns; j++) {
+        outputText += indent + indent + indent + '<th class="' + headerNames[j] + '-cell">';          
+        outputText += headerNames[j];
+        outputText += '</th>' + newLine;
     };
-    outputText += indent+indent+"</tr>"+newLine;
-    outputText += indent+"</thead>"+newLine;
-    outputText += indent+"<tbody>"+newLine;
-    for (var i=0; i < numRows; i++) {
-      var row = dataGrid[i];
-      var rowClassName = ""
-      if (i === numRows-1) {
-        rowClassName = ' class="lastRow"';
-      } else if (i === 0){
-        rowClassName = ' class="firstRow"';
-      }
-      outputText += indent+indent+"<tr"+rowClassName+">"+newLine;
-      for (var j=0; j < numColumns; j++) {
-        outputText += indent+indent+indent+'<td class="'+headerNames[j]+'-cell">';          
-        outputText += row[j]
-        outputText += '</td>'+newLine
-      };
-      outputText += indent+indent+"</tr>"+newLine;
+    outputText += indent + indent + "</tr>" + newLine;
+    outputText += indent + "</thead>" + newLine;
+    outputText += indent + "<tbody>" + newLine;
+    for (var i = 0; i < numRows; i++) {
+        var row = dataGrid[i];
+        var classes = [];
+        if (i === numRows-1) {
+            classes.push("lastRow");
+        } else if (i === 0){
+            classes.push("firstRow");
+        }
+        if (i % 2) {
+            classes.push("even");
+        } else {
+            classes.push("odd");
+        }
+        var rowClassName = "class=\"";
+        var classesLength = classes.length;
+        for (j in classes) {
+            rowClassName += classes[j];
+            if (j == 0 && classesLength > 1) {
+                rowClassName += " ";
+            }
+        }
+        rowClassName += "\"";
+        outputText += indent + indent + "<tr" + rowClassName + ">" + newLine;
+        for (var j = 0; j < numColumns; j++) {
+            outputText += indent + indent + indent + '<td class="' + headerNames[j] + '-cell">';          
+            outputText += row[j];
+            outputText += '</td>' + newLine;
+        };
+        outputText += indent + indent + "</tr>" + newLine;
     };
-    outputText += indent+"</tbody>"+newLine;
+    outputText += indent + "</tbody>" + newLine;
     outputText += "</table>";
-    
+
     return outputText;
-  },
- 
- 
-  //---------------------------------------
-  // HTML Table - Seattle Times styles
-  //---------------------------------------
- 
-  htmlSeaTimes: function (dataGrid, headerNames, headerTypes, indent, newLine) {
-    //inits...
-    var commentLine = "<!--";
-    var commentLineEnd = "-->";
-    var outputText = "";
-    var numRows = dataGrid.length;
-    var numColumns = headerNames.length;
-    
-    //begin render loop
-    outputText += "<table>"+newLine;
-    outputText += indent+"<thead>"+newLine;
-    outputText += indent+indent+"<tr>"+newLine;
-    
-    for (var j=0; j < numColumns; j++) {
-      outputText += indent+indent+indent+'<th class="'+headerNames[j]+'-cell">';          
-      outputText += headerNames[j];
-      outputText += '</th>'+newLine;
-    };
-    outputText += indent+indent+"</tr>"+newLine;
-    outputText += indent+"</thead>"+newLine;
-    outputText += indent+"<tbody>"+newLine;
-    for (var i=0; i < numRows; i++) {
-      var row = dataGrid[i];
-      var rowClassName = ""
-      if (i === numRows-1) {
-        rowClassName = ' class="lastRow"';
-      } else if (i === 0){
-        rowClassName = ' class="firstRow"';
-      }
-      outputText += indent+indent+"<tr"+rowClassName+">"+newLine;
-      for (var j=0; j < numColumns; j++) {
-        outputText += indent+indent+indent+'<td class="'+headerNames[j]+'-cell">';          
-        outputText += row[j]
-        outputText += '</td>'+newLine
-      };
-      outputText += indent+indent+"</tr>"+newLine;
-    };
-    outputText += indent+"</tbody>"+newLine;
-    outputText += "</table>";
-    
-    return outputText;
-  },
-  
+    },
+
     
   //---------------------------------------
   // JSON properties
